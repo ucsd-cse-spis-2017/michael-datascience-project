88468b5f9b2e8350d87732195a75560fd750997a
Michael Jaber <30811389+MichaelJaber@users.noreply.github.com>
2017-09-03 03:12:30 -0700

    Update network_runner.py

--- network_runner.py
+++ network_runner.py
@@ -1,8 +1,8 @@
 import mnist_loader
 
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-
 import network
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
 net = network.Network([784, 30, 10])
 
